1e8403852e88d9ed9d6651ec21354ec36029948a
Mariem Derbali <mariem.derbali20@gmail.com>
2024-07-12 14:05:33 +0100

    Enhanced Login CSS

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -9,6 +9,7 @@ import './Login.css';
 const LoginScreen = () => {
     const [Email, setEmail] = useState('');
     const [Password, setPassword] = useState('');
+    const [ShowPassword, setShowPassword] = useState(false); // State to toggle password visibility
     const loginState = useSelector((state) => state.loginUserReducer);
     const { loading, error, currentUser } = loginState; // Get currentUser from state
     const dispatch = useDispatch();
@@ -18,25 +19,59 @@ const LoginScreen = () => {
         dispatch(loginUser(user));
     };
 
+    // Toggle password visibility
+    const togglePasswordVisibility = () => {
+        setShowPassword(!ShowPassword);
+    };
+
     // Redirect to dashboard if currentUser exists (user is authenticated)
     if (currentUser) {
         return <Redirect to="/admin/dashboard" />;
     }
 
     return (
-             <div className='login-screen-box '>
-                <img src="https://icons.veryicon.com/png/o/miscellaneous/two-color-icon-library/user-286.png" className="avatar" alt="User Avatar" />
-                <h1>Connectez-vous</h1>
-                {loading && <Loading />}
-                {error && <Error error="Informations invalides" />}
-                <div>
-                    <input required type="email" placeholder="Email" className="form-control" value={Email} onChange={(e) => setEmail(e.target.value)} />
-                    <input required type="password" placeholder="Mot de passe" className="form-control" value={Password} onChange={(e) => setPassword(e.target.value)} />
-                    <button onClick={login} type="submit">Se connecter </button>
+        <div className='login-screen-box'>
+            <img src="/img/logo.png" className="loginlogo" alt="logo" />
+            <p className='title'>Connectez-vous à votre compte</p>
+            {loading && <Loading />}
+            {error && <Error error="Informations invalides" />}
+            <div>
+            <p className="label">E-mail</p>
 
+                <div className="input-container">
+                    <i style={{ color: '#977700' }}className="fas fa-envelope left-icon"></i>
+
+                    <input
+                        required
+                        type="email"
+                        placeholder="exemple@apisurv.com"
+                        className="form-control"
+                        value={Email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
                 </div>
+                <p className="label">Mote de passe</p>
+
+                <div className="input-container">
+                    <i style={{ color: '#977700' }} className="fas fa-lock left-icon"></i>
+                    <input
+                        required
+                        type={ShowPassword ? "text" : "password"} // Ensure password starts as hidden
+                        placeholder="********"
+                        className="form-control"
+                        value={Password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <i
+                        className={`fas ${ShowPassword ? 'fa-eye' : 'fa-eye-slash'} right-icon`}
+                        onClick={togglePasswordVisibility}
+                    ></i>
+                </div>
+                <button onClick={login} type="submit">Se connecter</button>
+                <p className='forgetPass'>Mot de passe oublié?</p>
             </div>
-     );
+        </div>
+    );
 };
 
 export default LoginScreen;
